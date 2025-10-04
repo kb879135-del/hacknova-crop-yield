a4443d533f4bf7ebfeeb6a0081dfd73c56e64b7e
kb879135-del <kb879135@gmail.com>
2025-10-04 03:17:02 -0700

    Update app.py

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "AI Crop Yield Prediction API is running 🚀"})
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    return jsonify({
+        "input": data,
+        "predicted_yield": "Dummy prediction (model will come later)"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
